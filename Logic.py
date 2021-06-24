ae59685c579a9b3f0506f413da8667d7ad901253
astroboi-lab <terry007x@korea.ac.kr>
2021-06-24 21:59:43 +0900

    get_correct_reads, get_mut_pos_seqs

--- Logic.py
+++ Logic.py
@@ -3,4 +3,71 @@ from astroboi_bio_tools.ToolLogic import ToolLogics
 
 
 class Logics(ToolLogics):
-    pass
+    def get_correct_reads(self, bg_dict, pe_list):
+        tot_read = 0.0
+        for pe_arr in pe_list:
+            alig_seq = pe_arr[2]
+            pe_read = float(pe_arr[-1])
+            if alig_seq in bg_dict:
+                bg_read = bg_dict[alig_seq][-1]
+                pe_arr.append(bg_read)
+                new_read = pe_read - float(bg_read)
+                if new_read < 0:
+                    new_read = 0.0
+                pe_arr.append(new_read)
+                tot_read += new_read
+            else:
+                pe_arr.append('NA')
+                pe_arr.append(pe_read)
+                tot_read += pe_read
+        return [pe_arr[:-1] + [100 - tot_read] if pe_arr[0] == 'WT' else pe_arr for pe_arr in pe_list]
+
+    def get_mut_pos_seqs(self, pe_list):
+        for pe_arr in pe_list:
+            ali_seq = pe_arr[2]
+            ref_seq = pe_arr[3]
+            mut_idx_arr = []
+            ali_cha_arr = []
+            ref_cha_arr = []
+
+            for i in range(len(ref_seq)):
+                if ali_seq[i] != ref_seq[i]:
+                    mut_idx_arr.append(i)
+                    ali_cha_arr.append(ali_seq[i])
+                    ref_cha_arr.append(ref_seq[i])
+
+            mut_pos_arr = []
+            ali_str_arr = []
+            ref_str_arr = []
+
+            tmp_ali_str = ""
+            tmp_ref_str = ""
+            for i in range(len(mut_idx_arr)):
+
+                if i == 0:
+                    mut_pos_arr.append(mut_idx_arr[i])
+                    tmp_ali_str += ali_cha_arr[i]
+                    tmp_ref_str += ref_cha_arr[i]
+
+                elif (mut_idx_arr[i] - 1) == mut_idx_arr[i - 1]:  # is_connected
+                    tmp_ali_str += ali_cha_arr[i]
+                    tmp_ref_str += ref_cha_arr[i]
+
+                else:  # new mut pos
+                    ali_str_arr.append(tmp_ali_str)
+                    ref_str_arr.append(tmp_ref_str)
+                    tmp_ali_str = ""
+                    tmp_ref_str = ""
+
+                    mut_pos_arr.append(mut_idx_arr[i])
+                    tmp_ali_str += ali_cha_arr[i]
+                    tmp_ref_str += ref_cha_arr[i]
+
+                if (i + 1) == len(mut_idx_arr):  # is_the_last
+                    ali_str_arr.append(tmp_ali_str)
+                    ref_str_arr.append(tmp_ref_str)
+
+            pe_arr.append(mut_pos_arr)
+            pe_arr.append(ref_str_arr)
+            pe_arr.append(ali_str_arr)
+        return pe_list
--- LogicPrep.py
+++ LogicPrep.py
@@ -1,6 +1,85 @@
 
 from astroboi_bio_tools.ToolLogicPrep import ToolLogicPreps
+import Util
 
 
 class LogicPreps(ToolLogicPreps):
-    pass
+    def make_list_to_dict_by_ele(self, data_list, ele_idx):
+        result_dict = {}
+        for val_arr in data_list:
+            key = val_arr[ele_idx]
+            if key in result_dict:
+                print("[ERROR] aligned_seq dupled", str(val_arr))
+            else:
+                result_dict.update({key: val_arr})
+        return result_dict
+
+    def get_bg_pe_list(self, path_arr):
+        util = Util.Utils()
+
+        bg_wt_list, bg_hd_list, pe_wt_list, pe_hd_list = [], [], [], []
+
+        try:
+            bg_wt_list += [
+                ['_del', 'Not_prime-edited'] + val_arr if int(val_arr[3]) > 0 else ['', 'Not_prime-edited'] + val_arr
+                for val_arr in util.read_tsv_ignore_N_line(path_arr[0])]
+
+            bg_wt_list = [[val_arr[0] + '_ins'] + val_arr[1:] if int(val_arr[6]) > 0 else val_arr for val_arr in
+                          bg_wt_list]
+            bg_wt_list = [[val_arr[0] + '_mut'] + val_arr[1:] if int(val_arr[7]) > 0 else val_arr for val_arr in
+                          bg_wt_list]
+            bg_wt_list = [['WT'] + val_arr[1:] if val_arr[0] == '' else val_arr for val_arr in bg_wt_list]
+        except Exception as err:
+            print("bg_wt path :", path_arr[0], "\n", err)
+            pass
+
+        try:
+            bg_hd_list += [['_del', 'Prime-edited'] + val_arr if int(val_arr[3]) > 0 else ['', 'Prime-edited'] + val_arr
+                           for val_arr in util.read_tsv_ignore_N_line(path_arr[1])]
+
+            bg_hd_list = [[val_arr[0] + '_ins'] + val_arr[1:] if int(val_arr[6]) > 0 else val_arr for val_arr in
+                          bg_hd_list]
+            bg_hd_list = [[val_arr[0] + '_mut'] + val_arr[1:] if int(val_arr[7]) > 0 else val_arr for val_arr in
+                          bg_hd_list]
+            bg_hd_list = [['Pure_PE'] + val_arr[1:] if val_arr[0] == '' else val_arr for val_arr in bg_hd_list]
+        except Exception as err:
+            print("bg_hdr path :", path_arr[1], "\n", err)
+            pass
+
+        bg_list = bg_wt_list + bg_hd_list
+        bg_wt_list.clear()
+        bg_hd_list.clear()
+        del bg_wt_list, bg_hd_list
+
+        try:
+            pe_wt_list += [
+                ['_del', 'Not_prime-edited'] + val_arr if int(val_arr[3]) > 0 else ['', 'Not_prime-edited'] + val_arr
+                for val_arr in util.read_tsv_ignore_N_line(path_arr[2])]
+
+            pe_wt_list = [[val_arr[0] + '_ins'] + val_arr[1:] if int(val_arr[6]) > 0 else val_arr for val_arr in
+                          pe_wt_list]
+            pe_wt_list = [[val_arr[0] + '_mut'] + val_arr[1:] if int(val_arr[7]) > 0 else val_arr for val_arr in
+                          pe_wt_list]
+            pe_wt_list = [['WT'] + val_arr[1:] if val_arr[0] == '' else val_arr for val_arr in pe_wt_list]
+        except Exception as err:
+            print("pe_wt path :", path_arr[2], "\n", err)
+            pass
+
+        try:
+            pe_hd_list += [['_del', 'Prime-edited'] + val_arr if int(val_arr[3]) > 0 else ['', 'Prime-edited'] + val_arr
+                           for val_arr in util.read_tsv_ignore_N_line(path_arr[3])]
+
+            pe_hd_list = [[val_arr[0] + '_ins'] + val_arr[1:] if int(val_arr[6]) > 0 else val_arr for val_arr in
+                          pe_hd_list]
+            pe_hd_list = [[val_arr[0] + '_mut'] + val_arr[1:] if int(val_arr[7]) > 0 else val_arr for val_arr in
+                          pe_hd_list]
+            pe_hd_list = [['Pure_PE'] + val_arr[1:] if val_arr[0] == '' else val_arr for val_arr in pe_hd_list]
+        except Exception as err:
+            print("pe_hdr path :", path_arr[3], "\n", err)
+            pass
+        pe_list = pe_wt_list + pe_hd_list
+        pe_wt_list.clear()
+        pe_hd_list.clear()
+        del pe_wt_list, pe_hd_list
+
+        return bg_list, pe_list
